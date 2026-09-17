6f5d24b7f89d21c51677133e5d81067e37f025f6
Angel Jojo <148966254+AngelJojo123@users.noreply.github.com>
2026-09-17 10:25:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours: ",min_value=0.0, max_value= 15.0, value=5.0)
 if st.button("Predict"):
   prediction=model.predict([[hours]])
+  probability = model.predict_proba([[hours]])
+  fail_probability = probability[0][0] * 100
+  pass_probability = probability[0][1] * 100
   if prediction[0]==1:
     st.success("Pass")
   else:
     st.error("Fail")
+st.write("Pass Probability:", round(pass_probability, 2), "%")
+st.write("Fail Probability:", round(fail_probability, 2), "%")
