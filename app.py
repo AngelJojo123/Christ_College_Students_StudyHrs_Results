1148a2c1d14432f5705c8966b5a3e13b028d6732
Angel Jojo <148966254+AngelJojo123@users.noreply.github.com>
2026-09-17 11:18:13 +0530

    Modify prediction to include attendance input
    Updated the prediction model to include attendance as a factor.

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import joblib
 model=joblib.load("LogisticRegression_study_hours_model.pkl")
 st.title("🎓Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours: ",min_value=0.0, max_value= 15.0, value=5.0)
+attendance=st.number_input("Enter Attendance: ",min_value=0.0, max_value= 100.0, value=75.0)
 if st.button("Predict"):
-  prediction=model.predict([[hours]])
-  probability = model.predict_proba([[hours]])
+  prediction=model.predict([[hours, attendance]])
+  probability = model.predict_proba([[hours, attendance]])
   fail_probability = probability[0][0] * 100
   pass_probability = probability[0][1] * 100
   if prediction[0]==1:
