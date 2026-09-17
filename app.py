48c355c1f68c39d12ea809f46c7b7ff5162ddabd
Angel Jojo <148966254+AngelJojo123@users.noreply.github.com>
2026-09-17 10:58:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 model=joblib.load("LogisticRegression_study_hours_model.pkl")
-st.title("Student Pass/Fail based on Study Hours")
+st.title("🎓Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours: ",min_value=0.0, max_value= 15.0, value=5.0)
 if st.button("Predict"):
   prediction=model.predict([[hours]])
@@ -9,7 +9,7 @@ if st.button("Predict"):
   fail_probability = probability[0][0] * 100
   pass_probability = probability[0][1] * 100
   if prediction[0]==1:
-    st.success("✓ Pass")
+    st.success("✅ Pass")
     st.write("Pass Probability:", round(pass_probability, 2), "%")
   else:
     st.error("❌ Fail")
