5360a8ca5be358736c34fea5ffc3d75de68e2c43
Angel Jojo <148966254+AngelJojo123@users.noreply.github.com>
2026-09-18 12:29:09 +0530

    Fix model filename in display_passfail_gradio.py

--- display_passfail_gradio.py
+++ display_passfail_gradio.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 # Load model
-model = joblib.load("Logistic_Regression_study_hours_model.pkl")
+model = joblib.load("LogisticRegression_study_hours_model.pkl")
 
 
 def predict_result(study_hours, attendance):
