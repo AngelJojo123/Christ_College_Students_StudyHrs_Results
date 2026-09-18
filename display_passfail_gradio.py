5bdc4945f28234538a26fde600e0d5336f59bf27
Angel Jojo <148966254+AngelJojo123@users.noreply.github.com>
2026-09-18 12:12:39 +0530

    Update display_passfail_gradio.py

--- display_passfail_gradio.py
+++ display_passfail_gradio.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 # Load model
-model = joblib.load("Student_Std_Hrs.pkl")
+model = joblib.load("LogisticRegression_study_hours_model.pkl")
 
 
 def predict_result(study_hours):
